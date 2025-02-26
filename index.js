de215566b98a818a7a7f13551edab3dff607670d
Esther-Sonia <esthersonia21@gmail.com>
2025-02-26 12:06:06 +0300

    adds functions using loop

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+  let thankYouMessage = []; 
+
+  for (let i = 0; i < names.length; i++) { 
+    thankYouMessage.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`); // Step 3 & 4
+  }
+
+  return thankYouMessage;
+}
+
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+
+function countDown(num) {
+  while (num >= 0) { 
+    console.log(num); 
+    num--; 
+  }
+}
+
+
+countDown(10);
